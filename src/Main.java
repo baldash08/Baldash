82924977b93b8f03c2685258dc84e83a4ae244da
Olzhas <olzhasbs03@gmail.com>
2021-02-21 01:28:59 +0600

    21 Feb 12:28am

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,6 @@ public class Main{
     public static void main(String[] args) {
         int c = -99999,b = 0;
         Scanner scanner = new Scanner(System.in);
-       /* String[] name = {"ALi" , "Galymzhan" , "Rauan" , "Islam" , "Sanzhar"};
-        int[] age = {16, 17 , 15 , 19, 18};
-        String[] address = {"Oskemen", "Almaty" , "Astana" , "Semey" , "Turkestan"};
-        for (int i = 0; i < age.length; i++) {
-            if (age[i] > c) {
-                c = age[i];
-                b = i;
-            }
-        }
-        System.out.println(name[b] + " " + c + "(" + b + ")" + " live in " + address[b]);
-   */
         String[] name = new String[10];
         int[] age = new int[10];
         String[] address = new String[10];
